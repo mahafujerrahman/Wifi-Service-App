17e9cfe59ee14da0956c3f8dc1ac5d2e9f2e465a
mahafujer <mahafujer1999@gmail.com>
2025-04-25 19:18:45 +0600

    update profile screen

--- lib/view/screen/profile/profileScreen.dart
+++ lib/view/screen/profile/profileScreen.dart
@@ -22,8 +22,7 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  Uint8List? _image;
-  File? selectedImage;
+
 
   @override
   Widget build(BuildContext context) {
@@ -212,86 +211,5 @@ class _ProfileScreenState extends State<ProfileScreen> {
         );
       },
     );
-  }
-
-  // Show image picker options (Gallery or Camera)
-  void showImagePickerOption(BuildContext context) {
-    showModalBottomSheet(
-      backgroundColor: AppColors.white,
-      context: context,
-      builder: (builder) {
-        return Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: SizedBox(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height / 4.2,
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      _pickImageFromGallery();
-                    },
-                    child: SizedBox(
-                      child: Column(
-                        children: [
-                          Icon(
-                            Icons.image,
-                            size: 50.w,
-                            color: AppColors.primaryColor,
-                          ),
-                          CustomText(text: 'Gallery')
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    onTap: () {
-                      _pickImageFromCamera();
-                    },
-                    child: SizedBox(
-                      child: Column(
-                        children: [
-                          Icon(Icons.camera_alt,
-                              size: 50.w, color: AppColors.primaryColor),
-                          CustomText(text: 'Camera')
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  // Pick image from gallery
-  Future _pickImageFromGallery() async {
-    final returnImage =
-    await ImagePicker().pickImage(source: ImageSource.gallery);
-    if (returnImage == null) return;
-    setState(() {
-      selectedImage = File(returnImage.path);
-      _image = File(returnImage.path).readAsBytesSync();
-    });
-    Get.back();
-  }
-
-  // Pick image from camera
-  Future _pickImageFromCamera() async {
-    final returnImage =
-    await ImagePicker().pickImage(source: ImageSource.camera);
-    if (returnImage == null) return;
-    setState(() {
-      selectedImage = File(returnImage.path);
-      _image = File(returnImage.path).readAsBytesSync();
-    });
-    Get.back();
   }
 }
